ce3b950b998435dd0b01c2e2ceaedbe19484ef4b
ARTrudeau <adolph.trudeau@mdanderson.org>
2023-10-26 15:36:35 -0500

    update datasource

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Houston Active Incidents Git Tracking
 
-Tracking City of Houston Fire/Police/EMS incident data from https://cohweb.houstontx.gov/ActiveIncidents/Combined.aspx
+Tracking City of Houston Fire/Police/EMS incident data from [911 Active Incident Dashboard](https://www.arcgis.com/apps/dashboards/c51f262fdf8d43b39b6770d1b3d6aa53)
 
 Many thanks to Simon Willison: [Git scraping: track changes over time by scraping to a Git repository](https://simonwillison.net/2020/Oct/9/git-scraping/)
---
+++ fetch-data.sh
@@ -0,0 +1,32 @@
+
+url="https://mycity2.houstontx.gov/pubgis01/rest/services/HEC/HEC_Active_Incidents/MapServer/0/query?"
+url=$url"f=json&"
+url=$url"cacheHint=true&"
+url=$url"resultOffset=0&"
+url=$url"resultRecordCount=500&"
+url=$url"where=1%3D1&"
+url=$url"orderByFields=CALL_TIME%20DESC&"
+url=$url"outFields=*&returnGeometry=false&"
+url=$url"spatialRel=esriSpatialRelIntersects&";
+
+
+curl "$url" \
+-s \
+--compressed | \
+jq '' > data.json
+
+#   -H 'Accept: */*' \
+#   -H 'Accept-Language: en-US,en;q=0.6' \
+#   -H 'Cache-Control: no-cache' \
+#   -H 'Connection: keep-alive' \
+#   -H 'Origin: https://www.arcgis.com' \
+#   -H 'Pragma: no-cache' \
+#   -H 'Referer: https://www.arcgis.com/' \
+#   -H 'Sec-Fetch-Dest: empty' \
+#   -H 'Sec-Fetch-Mode: cors' \
+#   -H 'Sec-Fetch-Site: cross-site' \
+#   -H 'Sec-GPC: 1' \
+#   -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36' \
+#   -H 'sec-ch-ua: "Chromium";v="118", "Brave";v="118", "Not=A?Brand";v="99"' \
+#   -H 'sec-ch-ua-mobile: ?0' \
+#   -H 'sec-ch-ua-platform: "macOS"' \
